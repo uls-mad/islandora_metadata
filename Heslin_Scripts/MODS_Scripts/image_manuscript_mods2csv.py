456fb28e7b94ff1c9be7dfba1f4e8b01714d47f6
Michael Bolam <mrbst20@pitt.edu>
2022-12-06 12:42:19 -0500

    Update image_manuscript_mods2csv.py

--- Heslin_Scripts/MODS_Scripts/image_manuscript_mods2csv.py
+++ Heslin_Scripts/MODS_Scripts/image_manuscript_mods2csv.py
@@ -161,7 +161,7 @@ new_csv = input('CSV has been created but headers need to be renamed and reindex
 fieldnames = ['identifier/pitt', 'title/titleInfo', 'creator', 'subject_geographic', 'subject_topic', 'namePart/subject', 'abstract', 'dateCreated/originInfo', 'normalized_date_qualifier','dateOther/display/originInfo',
              'dateOther/sort/originInfo', 'publication_status', 'copyright_status', '{http://www.cdlib.org/inside/diglib/copyrightMD}name/accessCondition','typeOfResource', 'languageTerm/code/language', 'title/relatedItem',
               'identifier/relatedItem', 'depositor', 'contributor', 'genre', 'form/physicalDescription', 'extent/physicalDescription', 'publisher/originInfo', 'note/prefercite/relatedItem', 'placeTerm/text/originInfo',
-              'note/series/relatedItem', 'note/subseries/relatedItem', 'note/container/relatedItem', 'dateCreated/relatedItem', 'identifier/local-asc/relatedItem', 'note/ownership/relatedItem']
+              'note/series/relatedItem', 'note/subseries/relatedItem', 'note/container/relatedItem', 'dateCreated/relatedItem', 'identifier/local-asc/relatedItem', 'note/ownership/relatedItem', 'identifier/source']
 
 df = pd.read_csv(csv_name) #===> Include the headers
 correct_df = df.copy()
@@ -181,7 +181,7 @@ correct_df2.rename(columns={'title/titleInfo': 'title', 'typeOfResource': 'type_
 'languageTerm/code/language': 'language', 'form/physicalDescription': 'format', 'extent/physicalDescription': 'extent', 'identifier/pitt' : 'identifier',
 'title/relatedItem': 'source_collection', 'dateCreated/originInfo': 'normalized_date', 'note/prefercite/relatedItem': 'source_citation', 'identifier/relatedItem': 'source_collection_id', 'note/container/relatedItem': 'source_container',
 'note/series/relatedItem': 'source_series', 'note/subseries/relatedItem': 'source_subseries', 'placeTerm/text/originInfo': 'pub_place',
-'abstract': 'description', 'namePart/subject': 'subject_name', '{http://www.cdlib.org/inside/diglib/copyrightMD}name/accessCondition' : 'rights_holder','note/address' : 'address', 'dateCreated/relatedItem' : "source_collection_date", 'identifier/local-asc/relatedItem' : 'source_collection_id', 'note/ownership/relatedItem' : 'source_ownership'}, inplace=True)
+'abstract': 'description', 'namePart/subject': 'subject_name', '{http://www.cdlib.org/inside/diglib/copyrightMD}name/accessCondition' : 'rights_holder', 'identifier/source' : 'source_id', 'note/address' : 'address', 'dateCreated/relatedItem' : "source_collection_date", 'identifier/local-asc/relatedItem' : 'source_collection_id', 'note/ownership/relatedItem' : 'source_ownership'}, inplace=True)
 
 #data cleaning
 nan_value = float("NaN")
