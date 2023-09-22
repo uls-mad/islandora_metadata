4fb5ba698fe79de561a70f138be5ed9f194efb3a
Tyrica Terry Kapral <tyt3@pitt.edu>
2023-09-22 16:58:15 -0400

    Add progress bar

--- Heslin_Scripts/MODS_Scripts/image_manuscript_mods2csv.py
+++ Heslin_Scripts/MODS_Scripts/image_manuscript_mods2csv.py
@@ -1,5 +1,5 @@
 import sys
-from tkinter import filedialog, messagebox
+from tkinter import filedialog, messagebox, ttk
 from tkinter import *
 from lxml import etree as ET
 import re
@@ -18,6 +18,8 @@ global destination
 destination = None
 global mods_ns
 mods_ns = '{http://www.loc.gov/mods/v3}'
+global records
+records = []
 
 
 """ Classes """
@@ -67,15 +69,16 @@ class DialogBox():
 
 
 class ModsElement:
-    def __init__(self, xpath, namespace, elementname, **kwargs):
+    def __init__(self, root, xpath, namespace, elementname, **kwargs):
+        self.root = root
         self.xpath = xpath
         self.namespace = namespace
         self.elementname = elementname
         self.additional_args = kwargs
 
     def get_element_value(self):
-        if root.find(self.xpath, self.namespace) is not None:
-            elementname = root.find(self.xpath, self.namespace).text
+        if self.root.find(self.xpath, self.namespace) is not None:
+            elementname = self.root.find(self.xpath, self.namespace).text
             return elementname
         else:
             elementname = ''
@@ -84,19 +87,19 @@ class ModsElement:
     def get_complex_element(self):
         value_list = []
         #if 'text' in self.additional_args.keys():
-        for element in root.findall(self.xpath, self.namespace):
+        for element in self.root.findall(self.xpath, self.namespace):
             if element is not None \
                 and element.text == self.additional_args['text']:
                 value_list.append(element.getparent().getprevious().text)
                 return value_list
 
     def get_element_attrib(self):
-        if root.find(self.xpath, self.namespace) is not None:
+        if self.root.find(self.xpath, self.namespace) is not None:
             elementattrib = 'yes'
             return elementattrib
         
 
-""" Helper functions """
+""" Helper Functions """
 
 # Show a given error and exit program
 def show_error(title=str, message=str):
@@ -146,6 +149,7 @@ def get_source(dialog_box=DialogBox, source_type=str):
     dialog_box.close()
 
 
+# Run file dialog box to get input filepath
 def run_source_dialog():
     # Create dialog box
     source_box = DialogBox(title="Select Source Type", dimensions="300x110")
@@ -165,6 +169,7 @@ def run_source_dialog():
     source_box.display()
 
 
+# Run file dialog box to get output filepath
 def get_destination():
     global destination
     files = [('CSV UTF-8', '*.csv')]
@@ -180,25 +185,184 @@ def special_field(element=ET.Element, tag=str):
             return True
     return False
 
+
 # Generate a list of an element's parents
-def get_parents(e):
+def get_parents(root=ET.Element, element=ET.Element):
     parent_list = []
-    while e.getparent() != root:
-        parent_list.append(e.getparent().tag.replace(mods_ns, ''))
-        e = e.getparent()
+    while element.getparent() != root:
+        parent_list.append(element.getparent().tag.replace(mods_ns, ''))
+        element = element.getparent()
     return parent_list
 
 
+# Get text values from given list of child elements
 def get_child_text(children=list):
     child_text = [child.text for child in children \
                   if child.text.strip() and child.text is not None]
-    print(child_text)
     return child_text
 
 
+# Remove newline characters, trailing whitespaces, and multiple spaces from text
 def remove_whitespaces(text):
     new_text = text.replace('\n    ', ' ').replace('\n', '').strip()
+    new_text = re.sub(r'\s+', ' ', new_text)
     return new_text
+
+
+""" Main Functions """
+
+def process_xml(file):
+    # Create an XML object that python can parse
+    xml_object = ET.parse(file)
+    # Get the root of that object
+    root = xml_object.getroot()
+    # Initialize dictionary for elements
+    elements = {}
+
+    # Create dictionary of dictionaries from parsed XML data
+    # Top-level dictionary: Key = Xpath, value = child dictionary
+    # Child dictionary: Key = field value, value = list of parent element(s)
+    for element in root.xpath('.//*'):
+        tag = element.tag
+        text = element.text
+        type_attribute = element.attrib.get('type')
+        
+        # Check that current element and its parent is not subject 
+        # and that the element text is not empty
+        if not special_field(element, tag) and text is not None:
+            # Add XML dictionary with list of all parent elements
+            if type_attribute:
+                elements.setdefault(f'{tag}/{type_attribute}', []).append(
+                    {remove_whitespaces(text):
+                        [e for e in get_parents(root, element)]})
+            else:
+                elements.setdefault(tag, []).append(
+                    {remove_whitespaces(text):
+                        [e for e in get_parents(root, element)]})
+    
+    # Create dictionary from parsed XML data in elements dictionary
+    record = {}
+
+    for e1, e2 in elements.items():
+        for value_tag in e2:
+            for value, tag in value_tag.items():
+                # Check if value has non-whitespace characters/is not empty
+                if not value.strip():
+                    continue
+                if tag:
+                    record.setdefault(f"{e1.replace(mods_ns, '')}/{tag[-1]}", 
+                        []).append(value.replace('\r', ' '))
+                else:
+                    record.setdefault(e1.replace(mods_ns, ''),
+                        []).append(value.replace('\r', ' '))
+
+    # Get accessCondition elements
+    publication_status = ""
+    copyright_status = ""
+
+    for access_conditions in root.iterfind('mods:accessCondition', 
+                                            namespaces['mods_ns']):
+        for copyrights in access_conditions.iterfind(
+            'copyrightMD:copyright', namespaces['copyright_ns']):
+            publication_status = copyrights.attrib['publication.status']
+            copyright_status = copyrights.attrib['copyright.status']
+
+    # Get subject elements
+    for subject in root.iterfind('mods:subject', namespaces['mods_ns']):
+        children = subject.getchildren()
+        if not children:
+            continue
+        subject_field = f"subject_{children[0].tag.replace(mods_ns, '')}"
+        record.setdefault(subject_field, [])
+        values = '--'.join(get_child_text(children))
+        if values:
+            record[subject_field].append(values)
+        if subject_field == 'subject_name':
+            grandchildren = children[0].getchildren()
+            if grandchildren:
+                record[subject_field] += (get_child_text(grandchildren))
+        
+    # Create a dictionary for each targeted namePart roleTerm
+    nameParts = {
+        "creator": [],
+        "contributor": [],
+        "depositor": []
+    }
+
+    # Get all namePart elements and add values to corresponding role list
+    for field in root.findall('.//mods:namePart', namespaces['mods_ns']):
+        try:
+            # below throws IndexError: list index out of range
+            roleTerm = field.getnext().getchildren()[0].text
+            nameParts[roleTerm].append(field.text)
+        except:
+            pass
+
+    # Create a MODS element from Xpath
+    date_qualifier = ModsElement(
+        root=root,
+        xpath=".//mods:dateCreated[@qualifier='approximate'][@encoding='iso8601'][@keyDate='yes']", 
+        namespace=namespaces['mods_ns'], 
+        elementname='date_qualifier'
+        )
+    
+    # Add fields to second XML dictionary
+    record.setdefault('copyright_status', copyright_status)
+    record.setdefault('publication_status', publication_status)
+    record.setdefault('contributor', nameParts['contributor'])
+    record.setdefault('creator', nameParts['creator'])
+    record.setdefault('depositor', nameParts['depositor'])
+    record.setdefault('normalized_date_qualifier',
+                        date_qualifier.get_element_attrib())
+    
+    # Convert field value from lists to strings
+    for field, value in record.items(): 
+        if type(value) is list:
+            record[field] = '; '.join(value)
+
+    # Add record dictionary to records
+    records.append(record)
+
+
+def manage_processor(files=list, total_files=int, progress=int, 
+                  progress_var=DoubleVar, processed_label=Label):
+    if progress < total_files:
+        # Process the file
+        process_xml(files[progress])
+
+        # Update progress
+        progress += 1
+        progress_var.set(int((progress / total_files) * 100))
+
+        # Update processed label
+        text = f"{progress}/{total_files} files"
+        processed_label.config(text=text)
+        processed_label.update_idletasks() 
+
+        # Schedule the next file processing
+        root.after(1, manage_processor, files, total_files, progress, 
+                   progress_var, processed_label)
+    else:
+        # Processing is complete
+        processing_complete_label.config(text="Complete!")
+
+
+# Function to start processing files
+def start_processing(root=Tk, files=list):
+    # Initialize progress variable
+    progress = 0
+
+    # Update root to display components
+    root.update_idletasks() 
+
+    # Create and place processed label
+    processed_label = Label(root, text="0/0")
+    processed_label.pack()
+    processed_label.update_idletasks() 
+    
+    # Start processing files
+    manage_processor(files=files, total_files = len(files), progress=progress, 
+                  progress_var=progress_var, processed_label=processed_label)
 
 
 """ Driver Code """
@@ -236,121 +400,29 @@ if __name__ == "__main__":
     os.chdir(source)
 
     # Get list of XML files in directory
-    list_of_files = glob.glob('*.xml')
+    file_list = glob.glob('*.xml')
 
-    # Generate a list of records (i.e., dictionaries) from XML files
-    records = []
+    # Get records by parsing and processing data in XML files
+    root = Tk()
+    root.title("Processing Files")
+    root.geometry('255x125')
+    root.attributes("-topmost", True)
 
-    # TO DO: Create process_files() function
-    for file in list_of_files:
-        # Create an XML object that python can parse
-        xml_object = ET.parse(file)
-        # Get the root of that object
-        root = xml_object.getroot()
-        # Initialize dictionary for elements
-        elements = {}
+    # Create a progress bar
+    progress_var = DoubleVar()
+    progress_bar = ttk.Progressbar(root, variable=progress_var, 
+                                   maximum=100, len=200)
+    progress_bar.pack(pady=20)
 
-        # Create dictionary of dictionaries from parsed XML data
-        # Top-level dictionary: Key = Xpath, value = child dictionary
-        # Child dictionary: Key = field value, value = list of parent element(s)
-        for element in root.xpath('.//*'):
-            tag = element.tag
-            text = element.text
-            type_attribute = element.attrib.get('type')
-            
-            # Check that current element and its parent is not subject 
-            # and that the element text is not empty
-            if not special_field(element, tag) and text is not None:
-                # Add XML dictionary with list of all parent elements
-                if type_attribute:
-                    elements.setdefault(f'{tag}/{type_attribute}', []).append(
-                        {remove_whitespaces(text):
-                         [e for e in get_parents(element)]})
-                else:
-                    elements.setdefault(tag, []).append(
-                        {remove_whitespaces(text):
-                            [e for e in get_parents(element)]})
-        
-        # Create dictionary from parsed XML data in elements dictionary
-        record = {}
+    # Label to display processing status
+    processing_complete_label = Label(root, text="Processing...")
+    processing_complete_label.pack()
 
-        for e1, e2 in elements.items():
-            for value_tag in e2:
-                for value, tag in value_tag.items():
-                    # Check if value has non-whitespace characters/is not empty
-                    if not value.strip():
-                        continue
-                    if tag:
-                        record.setdefault(f"{e1.replace(mods_ns, '')}/{tag[-1]}", 
-                            []).append(value.replace('\r', ' '))
-                    else:
-                        record.setdefault(e1.replace(mods_ns, ''),
-                            []).append(value.replace('\r', ' '))
+    # Start processing files automatically
+    start_processing(root=root, files=file_list)
 
-        # Get accessCondition elements
-        for access_conditions in root.iterfind('mods:accessCondition', 
-                                               namespaces['mods_ns']):
-            for copyrights in access_conditions.iterfind(
-                'copyrightMD:copyright', namespaces['copyright_ns']):
-                publication_status = copyrights.attrib['publication.status']
-                copyright_status = copyrights.attrib['copyright.status']
-
-        # Get subject elements
-        for subject in root.iterfind('mods:subject', namespaces['mods_ns']):
-            children = subject.getchildren()
-            if not children:
-                continue
-            subject_field = f"subject_{children[0].tag.replace(mods_ns, '')}"
-            record.setdefault(subject_field, [])
-            values = '--'.join(get_child_text(children))
-            if values:
-                record[subject_field].append(values)
-            if subject_field == 'subject_name':
-                grandchildren = children[0].getchildren()
-                if grandchildren:
-                    record[subject_field] += (get_child_text(grandchildren))
-            
-        
-        # Create a dictionary for each targeted namePart roleTerm
-        nameParts = {
-            "creator": [],
-            "contributor": [],
-            "depositor": []
-        }
-
-        # Get all namePart elements and add values to corresponding role list
-        for field in root.findall('.//mods:namePart', namespaces['mods_ns']):
-            try:
-                # below throws IndexError: list index out of range
-                roleTerm = field.getnext().getchildren()[0].text
-                nameParts[roleTerm].append(field.text)
-            except:
-                pass
-
-        # Create a MODS element from Xpath
-        date_qualifier = ModsElement(
-            xpath=".//mods:dateCreated[@qualifier='approximate'][@encoding='iso8601'][@keyDate='yes']", 
-            namespace=namespaces['mods_ns'], 
-            elementname='date_qualifier'
-            )
-        
-        # Add fields to second XML dictionary
-        record.setdefault('copyright_status', copyright_status)
-        record.setdefault('publication_status', publication_status)
-        record.setdefault('contributor', nameParts['contributor'])
-        record.setdefault('creator', nameParts['creator'])
-        record.setdefault('depositor', nameParts['depositor'])
-        record.setdefault('normalized_date_qualifier',
-                             date_qualifier.get_element_attrib())
-        
-        # Convert field value from lists to strings
-        for field, value in record.items(): 
-            if type(value) is list:
-                record[field] = '; '.join(value)
-
-        # Add record dictionary to records
-        records.append(record)
-
+    root.mainloop()
+    
     # Convert list of dictionaries to DataFrame
     df = pd.DataFrame.from_dict(records)
 
@@ -368,8 +440,8 @@ if __name__ == "__main__":
     df.replace({'': nan_value, '; ': nan_value, '; ; ': nan_value}, inplace=True)
     df.dropna(how='all', axis=1, inplace=True)
 
+
+    """ Save Data """
+
     # Write DataFrame to CSV file
     df.to_csv(destination, index=False, header=True, encoding='utf-8')
-
-    # Notify user that program completed successfully
-    messagebox.showinfo(title="Success", message="Success!")
